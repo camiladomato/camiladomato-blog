7cc5efe270b025cb3ab171791008d75d3306aaaa
Camila Domato <camiladomato@gmail.com>
2026-05-13 20:00:56 -0300

    add conf tina

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '358081785464601bb9cd90b301bcd0bed2a73e26', queries,  });
+export const client = createClient({ cacheDir: 'C:/proyectos/camila-domato/mi-blog-astro/former-flare/tina/__generated__/.cache/1778712422536', url: 'https://content.tinajs.io/2.4/content/3e3113ec-70f0-42a1-9d4b-431dd3815a73/github/main', token: '358081785464601bb9cd90b301bcd0bed2a73e26', queries,  });
 export default client;
   
